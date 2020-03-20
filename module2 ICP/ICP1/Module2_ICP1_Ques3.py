a4634ad1594328d1419c3878b2677c4dae15e59f
udayabyreddy <54335429+udayabyreddy@users.noreply.github.com>
2020-03-20 16:28:53 -0500

    MODULE2 ICP1

---
+++ module2 ICP/ICP1/Module2_ICP1_Ques3.py
@@ -0,0 +1,36 @@
+import pandas
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.utils import to_categorical
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+# load dataset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+sc = StandardScaler()
+dataset = pd.read_csv("Breas Cancer.csv", header=0).values
+x=dataset[:,2:30]
+sc.fit(x)
+x_norm=sc.transform(x)
+target = dataset[:,1]
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(target)
+encoded_Y = encoder.transform(target)
+print(encoded_Y)
+
+import numpy as np
+X_train, X_test, Y_train, Y_test = train_test_split(x_norm, encoded_Y,
+                                                    test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(20, input_dim=28, activation='relu')) # hidden layer
+my_first_nn.add(Dense(8, activation='relu')) #hiddenlayer 2
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100, verbose=0,
+                                     initial_epoch=0)
+print(my_first_nn.summary())
+print(my_first_nn.evaluate(X_test, Y_test, verbose=0))
